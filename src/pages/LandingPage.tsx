db2905c480f54fbe4469d062028243a6ce664a39
rishav-bhardwaz <rishav76000@gmail.com>
2024-09-06 10:21:42 +0530

    update

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -11,17 +11,17 @@ import Gallery from "../components/Gallery";
 
 const LandingPage: FunctionComponent = () => {
   return (
-    <div className="w-full relative bg-ppp-white overflow-hidden flex flex-col items-end justify-start pt-14 pb-[125px] pl-[63px] pr-16 box-border gap-11 leading-[normal] tracking-[normal] mq900:gap-[22px] mq1300:pl-[31px] mq1300:pr-8 mq1300:box-border">
-      <section className="flex flex-row items-start justify-end py-0 px-9 box-border max-w-full text-left text-5xl text-gray-200 font-outfit">
-        <div className="w-full flex flex-col items-start justify-start gap-[33px] max-w-full mq900:gap-4">
+    <div className="w-full relative bg-ppp-white overflow-hidden flex flex-col items-end justify-start pt-14 pb-[125px] px-[63px] box-border gap-11 leading-[normal] tracking-[normal] lg:px-[31px] md:px-8 md:gap-[22px]">
+      <section className="flex flex-row items-start justify-end py-0 px-9 box-border max-w-full text-left text-5xl text-gray-200 font-outfit md:px-4">
+        <div className="w-full flex flex-col items-start justify-start gap-[33px] max-w-full lg:gap-[22px]">
           <FrameComponent1 />
-          <div className="self-stretch flex flex-row items-start justify-start gap-[73px] max-w-full flex-wrap mq900:gap-9 mq450:gap-[18px]">
+          <div className="self-stretch flex flex-row items-start justify-start gap-[73px] max-w-full flex-wrap lg:gap-9 md:gap-[18px]">
             <Testimonial />
-            <div className="flex-1 flex flex-col items-start justify-start gap-[42px] min-w-[300px] max-w-full mq900:min-w-full mq450:gap-[21px]">
-              <div className="self-stretch relative max-w-full mq900:min-h-[895px]">
+            <div className="flex-1 flex flex-col items-start justify-start gap-[42px] min-w-[300px] max-w-full lg:min-w-[450px] md:min-w-full md:gap-[21px]">
+              <div className="self-stretch relative max-w-full lg:min-h-[700px]">
                 {/* Adjusted Container */}
                 <div
-                  className="absolute top-[107px] left-[139px] w-[645px] max-w-full overflow-hidden flex flex-row items-start justify-center pt-7 pb-[27px] pl-[21px] pr-5 box-border bg-cover bg-no-repeat bg-[top] z-[1] mq1300:left-[50px] mq900:left-[0px] mq900:w-full"
+                  className="absolute top-[107px] left-[139px] w-[645px] max-w-full overflow-hidden flex flex-row items-start justify-center pt-7 pb-[27px] pl-[21px] pr-5 box-border bg-cover bg-no-repeat bg-[top] z-[1] lg:left-[50px] md:left-0 md:w-full"
                   style={{
                     backgroundImage: "url('/public/wheel1-2@3x.png')",
                     backgroundSize: "cover",
@@ -29,8 +29,8 @@ const LandingPage: FunctionComponent = () => {
                     height: "auto",
                   }}
                 >
-                  <div className="w-full max-w-[400px] rounded-[20px] bg-ppp-white border-ppp-grey border-[1px] border-solid box-border flex flex-col items-start justify-start py-[38px] px-[39px] gap-4 mq900:py-[30px] mq900:px-[20px]">
-                    <div className="relative font-semibold mq450:text-lgi">
+                  <div className="w-full max-w-[400px] rounded-[20px] bg-ppp-white border-ppp-grey border-[1px] border-solid box-border flex flex-col items-start justify-start py-[38px] px-[39px] gap-4 lg:px-5 md:px-3 md:py-[30px]">
+                    <div className="relative font-semibold md:text-lg">
                       Register For Demo Class
                     </div>
                     <div className="self-stretch h-px relative border-ppp-grey border-t-[1px] border-solid box-border" />
@@ -61,7 +61,15 @@ const LandingPage: FunctionComponent = () => {
                         fieldText="9876543210"
                       />
                     </div>
-                    <Buttondefault />
+                    {/* Wrap the Buttondefault component with an anchor link */}
+                    <a
+                      href="https://whatsapp.com/channel/0029VaCvtuLK0IBclhbqYi2Z"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="w-full"
+                    >
+                      <Buttondefault />
+                    </a>
                   </div>
                 </div>
                 {/* Adjust other sections */}
@@ -69,18 +77,18 @@ const LandingPage: FunctionComponent = () => {
                   <div className="absolute h-full top-0 bottom-0 left-[12px] [filter:blur(400px)] rounded-[50%] bg-firebrick w-[895px]" />
                   {/* Additional stars */}
                   <img
-                    className="absolute top-[24px] left-[64px] w-[29px] h-[29px] z-[1] mq900:w-[20px] mq900:h-[20px]"
+                    className="absolute top-[24px] left-[64px] w-[29px] h-[29px] z-[1] md:w-[20px] md:h-[20px]"
                     loading="lazy"
                     alt=""
                     src="/star-1.svg"
                   />
                   <img
-                    className="absolute top-[194px] left-[79px] w-3.5 h-3.5 z-[1] mq900:w-2.5 mq900:h-2.5"
+                    className="absolute top-[194px] left-[79px] w-3.5 h-3.5 z-[1] md:w-2.5 md:h-2.5"
                     alt=""
                     src="/star-2.svg"
                   />
                   <img
-                    className="absolute top-[431px] left-0 w-[58px] h-[58px] z-[1] mq900:w-[40px] mq900:h-[40px]"
+                    className="absolute top-[431px] left-0 w-[58px] h-[58px] z-[1] md:w-[40px] md:h-[40px]"
                     loading="lazy"
                     alt=""
                     src="/star-4.svg"
