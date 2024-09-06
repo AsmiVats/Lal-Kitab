499dbb566f782cb7d5a7d6f1f1fbc0178b9cc01d
Asmi Vats <asmivats15@gmail.com>
2024-09-06 10:50:44 +0530

    course

--- src/components/LinkList.tsx
+++ src/components/LinkList.tsx
@@ -7,32 +7,33 @@ export type LinkListType = {
 const LinkList: FunctionComponent<LinkListType> = ({ className = "" }) => {
   return (
     <section
-      className={`flex flex-row items-start justify-end pt-0 px-9 pb-[148.9px] box-border max-w-full text-center text-45xl text-color font-alegreya mq900:pb-[63px] mq900:box-border mq1300:pb-[97px] mq1300:box-border ${className}`}
+      className={`flex flex-col items-center justify-start pt-0 px-9 pb-[148.9px] box-border max-w-full text-center text-45xl text-color font-alegreya mq900:pb-[63px] mq1300:pb-[97px] ${className}`}
     >
-      <div className="w-[1712px] flex flex-col items-start justify-start gap-[18px] max-w-full">
-        <div className="self-stretch flex flex-col items-start justify-start gap-0.5">
-          <h1 className="m-0 self-stretch h-[87px] relative text-inherit uppercase font-bold font-[inherit] inline-block mq900:text-32xl mq450:text-19xl">
+      <div className="w-full flex flex-col items-center justify-start gap-[18px] max-w-full">
+        {/* Heading Section */}
+        <div className="self-stretch flex flex-col items-center justify-start gap-0.5 text-center">
+          <h1 className="m-0 text-[inherit] uppercase font-bold text-45xl sm:text-32xl sm:text-center">
             Courses
           </h1>
-          <h1 className="m-0 self-stretch h-[228px] relative text-65xl uppercase font-bold font-[inherit] text-black whitespace-pre-wrap inline-block mq900:text-23xl mq450:text-6xl">
+          <h1 className="m-2 w-1/2 text-[inherit] uppercase font-bold text-black text-33xl sm:text-23xl sm:leading-tight">
             Get Latest Forecast and Analysis for Success
           </h1>
         </div>
-        <div className="self-stretch flex flex-row items-start justify-start py-0 pl-11 pr-[45px] box-border max-w-full mq1300:pl-[22px] mq1300:pr-[22px] mq1300:box-border">
-          <div className="flex-1 flex flex-row items-start justify-between max-w-full gap-5 mq1650:flex-wrap">
-            <img
-              className="self-stretch w-[677px] relative max-h-full object-cover min-h-[873px] max-w-full"
-              loading="lazy"
-              alt=""
-              src="/photo20240818163614-2@2x.png"
-            />
-            <img
-              className="self-stretch w-[677px] relative max-h-full object-cover min-h-[873px] max-w-full"
-              loading="lazy"
-              alt=""
-              src="/photo20240818163614-2@2x.png"
-            />
-          </div>
+
+        {/* Images Section */}
+        <div className="flex flex-row items-start justify-between w-full  flex-wrap">
+          <img
+            className="w-1/2 object-cover "
+            loading="lazy"
+            alt="Course Image 1"
+            src="/photo20240818163614-2@2x.png"
+          />
+          <img
+            className="w-1/2 object-cover "
+            loading="lazy"
+            alt="Course Image 2"
+            src="/photo20240818163614-2@2x.png"
+          />
         </div>
       </div>
     </section>
